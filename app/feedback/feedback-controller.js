3981403f052e7c4b802e6ec78028be9bd4a74854
Eugene Poddubny <epoddubny@outlook.com>
2016-03-06 01:29:20 +0300

    Feedback form validation

--- app/feedback/feedback-controller.js
+++ app/feedback/feedback-controller.js
@@ -4,16 +4,16 @@
 'use strict';
 
 
-angular.module('controllers').controller('FeedbackController', ['$scope', '$rootScope', '$filter', 'FeedbackService',
-    'Notification',
-    function($scope, $rootScope, $filter, FeedbackService, Notification) {
+angular.module('controllers').controller('FeedbackController', ['$scope', '$rootScope', '$filter', '$route',
+    'FeedbackService', 'Notification',
+    function($scope, $rootScope, $filter, $route, FeedbackService, Notification) {
 
-        $rootScope.title = $filter('translate')('FEEDBACK');
+        $rootScope.title = $filter('translate')('FEEDBACK_TITLE');
 
         $scope.sendFeedback = function() {
             FeedbackService.send($scope.feedback).success(function(data) {
                 Notification.success($filter('translate')('MESSAGE_SENT'));
-                $scope.feedback = {};
+                $route.reload();
             }).error(function(status, data) {
                 Notification.error($filter('translate')('ERROR') + status);
             });
